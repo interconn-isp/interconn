184dee077df866687a1a5c66a56cfd667d619a33
Alessandro Desantis <desa.alessandro@gmail.com>
2016-04-25 22:47:08 +0200

    Enable Lograge in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,4 +56,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Enable Lograge for better logging.
+  config.lograge.enabled = true
 end

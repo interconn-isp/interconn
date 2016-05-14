20a5f5d8b61e9d239dc7ef6ac8d2290c166be954
Alessandro Desantis <desa.alessandro@gmail.com>
2016-05-14 19:23:11 +0200

    Add WHDSL pricing plans

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: 'high_voltage/pages#show', id: 'home'
 
   get '/pricing/wadsl', to: 'high_voltage/pages#show', id: 'pricing/wadsl'
+  get '/pricing/whdsl', to: 'high_voltage/pages#show', id: 'pricing/whdsl'
 end

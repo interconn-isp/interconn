bb8e1c468e5545d6dac5163a88212238fa8ac061
Alessandro Desantis <desa.alessandro@gmail.com>
2016-05-15 17:00:47 +0200

    Add VoIP pricing page

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   get '/pricing/whdsl', to: 'high_voltage/pages#show', id: 'pricing/whdsl'
   get '/pricing/adsl', to: 'high_voltage/pages#show', id: 'pricing/adsl'
   get '/pricing/fiber', to: 'high_voltage/pages#show', id: 'pricing/fiber'
+  get '/pricing/voip', to: 'high_voltage/pages#show', id: 'pricing/voip'
 end

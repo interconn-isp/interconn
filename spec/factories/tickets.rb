c348fe325354ae2515cc2d0db9b16c46bf67c8ad
Alessandro Desantis <desa.alessandro@gmail.com>
2016-09-21 22:44:50 +0200

    Test the Ticket model

--- spec/factories/tickets.rb
+++ spec/factories/tickets.rb
@@ -1,9 +1,12 @@
 FactoryGirl.define do
   factory :ticket do
-    full_name "MyString"
-    phone_number "MyString"
-    email "MyString"
-    subject "MyString"
-    body "MyText"
+    full_name { Faker::Name.name }
+    email { Faker::Internet.email }
+    subject 'Nothing works!'
+    body { Faker::Lorem.paragraph }
+
+    trait :with_phone_number do
+      phone_number { Faker::Phone.number }
+    end
   end
 end
